7f530f8330b29055f2d750bcec2416b298e76777
Vladimir Pasquier <vpasquier@nuxeo.com>
2014-01-30 17:23:26 +0100

    NXIO-48: add POST Box folder endpoint

--- src/main/java/com/nuxeo/box/api/folder/BoxFolderObject.java
+++ src/main/java/com/nuxeo/box/api/folder/BoxFolderObject.java
@@ -12,14 +12,18 @@
  * Lesser General Public License for more details.
  *
  * Contributors:
- *     vpasquier<vpasquier@nuxeo.com>
+ *     vpasquier <vpasquier@nuxeo.com>
  *     dmetzler <dmetzler@nuxeo.com>
  */
 package com.nuxeo.box.api.folder;
 
+import com.box.boxjavalibv2.dao.BoxFolder;
 import com.box.boxjavalibv2.exceptions.BoxJSONException;
+import com.box.boxjavalibv2.jsonparsing.BoxJSONParser;
+import com.box.boxjavalibv2.jsonparsing.BoxResourceHub;
 import com.nuxeo.box.api.folder.io.BoxFolderAdapter;
 import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.model.NoSuchDocumentException;
@@ -28,6 +32,7 @@ import org.nuxeo.ecm.webengine.model.impl.AbstractResource;
 import org.nuxeo.ecm.webengine.model.impl.ResourceTypeImpl;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,7 +44,7 @@ import javax.ws.rs.core.MediaType;
  * @since 5.9.2
  */
 @WebObject(type = "folder")
-@Produces({ "application/json+nxentity", MediaType.APPLICATION_JSON })
+@Produces({ MediaType.APPLICATION_JSON })
 public class BoxFolderObject extends AbstractResource<ResourceTypeImpl> {
 
     @GET
@@ -50,11 +55,23 @@ public class BoxFolderObject extends AbstractResource<ResourceTypeImpl> {
     @GET
     @Path("{folderId}")
     public Object doGetFolder(@PathParam("folderId")
-    String folderId) throws NoSuchDocumentException, ClientException, BoxJSONException {
-        DocumentModel folder = ctx.getCoreSession().getDocument(new IdRef(folderId));
-        BoxFolderAdapter folderAdapter = folder.getAdapter(BoxFolderAdapter.class);
-        folderAdapter.newBoxInstance(ctx.getCoreSession());
+    final String folderId) throws NoSuchDocumentException, ClientException, BoxJSONException {
+        final CoreSession session = ctx.getCoreSession();
+        final DocumentModel folder = session.getDocument(new IdRef(folderId));
+        final BoxFolderAdapter folderAdapter = folder.getAdapter(BoxFolderAdapter.class);
+        folderAdapter.newBoxInstance(session);
         return folderAdapter.getJSONBoxFolder();
     }
 
+    @POST
+    public Object doPostFolder(String jsonBoxFolder) throws ClientException, BoxJSONException {
+        BoxFolder boxFolder = new BoxJSONParser(new BoxResourceHub()).parseIntoBoxObject(jsonBoxFolder, BoxFolder.class);
+        final CoreSession session = ctx.getCoreSession();
+        final DocumentModel parentDoc = session.getDocument(new IdRef(boxFolder.getParent().getId()));
+        DocumentModel newFolder = session.createDocumentModel(parentDoc.getPathAsString(), boxFolder.getId(), "Folder");
+        newFolder = session.createDocument(newFolder);
+        final BoxFolderAdapter folderAdapter = newFolder.getAdapter(BoxFolderAdapter.class);
+        folderAdapter.newBoxInstance(session);
+        return folderAdapter.getJSONBoxFolder();
+    }
 }
